d4e7cbab77a3109da88d515640a85b9b83d4ad0c
kensplanet <kensplanet@yahoo.com>
2016-06-28 21:55:01 -0400

    Reorganize the project using React - https://github.com/kensplanet/dev-dictionary/issues/9

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,16 @@
-/**
- * Created by kensplanet on 28/06/16.
- */
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <h1>Dev Dictionary</h1>
+        );
+    }
+}
+
+export default SearchBar;
