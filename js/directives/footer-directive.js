9956a8527d7aeda54b669dd418397e50b69d27f6
kensplanet <kensplanet@yahoo.com>
2016-06-28 20:56:45 -0400

    Reorganize the project using React - https://github.com/kensplanet/dev-dictionary/issues/9

--- js/directives/footer-directive.js
+++
@@ -1,9 +0,0 @@
-angular.module("app").directive("footer", [function () {
-    "use strict";
-    return {
-        link: function () {
-        },
-        templateUrl: "templates/footer.html"
-    };
-}
-]);
--- js/modules.js
+++
@@ -1,6 +0,0 @@
-angular.module('app', [
-    'ngRoute',
-    'ui.bootstrap',
-    'ui.bootstrap.tpls',
-    'ngSanitize'
-]);
