68b490cce7c3582a18c577ee12f50cae4880f4bc
kenneth.crasta <kenneth.crasta@ethoca.com>
2016-08-22 12:04:37 -0400

    Enhancements

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <h1>Dev Dictionary</h1>
+        );
+    }
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components.header';
 import SearchBar from './components/searchBar';
 
 class App extends Component {
@@ -10,7 +11,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <SearchBar />
+                <Header />
             </div>
         );
     }
