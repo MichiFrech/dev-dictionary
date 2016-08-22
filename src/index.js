22a464de7602fb485a9fa014ac2e868b803a1d88
kenneth.crasta <kenneth.crasta@ethoca.com>
2016-08-22 12:47:08 -0400

    Enhancements

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header';
 import SearchBar from './containers/searchBar';
+import $ from 'jquery';
 
 class App extends Component {
     constructor(props) {
