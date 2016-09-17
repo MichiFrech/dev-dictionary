54b376b5ae9e95eefc55225890b46bab8a752459
kensplanet <kensplanet@yahoo.com>
2016-09-17 15:54:18 -0400

    Reorganize the project using React - https://github.com/kensplanet/dev-dictionary/issues/9

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+var path = require('path');
+var webpackUglifyJsPlugin = require('webpack-uglify-js-plugin');
+
 module.exports = {
     entry: [
         './src/index.js'
@@ -8,28 +11,42 @@ module.exports = {
         filename: 'bundle.js'
     },
     module: {
-            loaders: [
-            {
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                include: [
-                    __dirname
-                ],
-                loader: 'css-loader'
-            },
-            {
-                test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-                loader: 'file-loader'
-            },
-            {
-                test: /bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
-                loader: 'imports?jQuery=jquery'
-            }
-        ]
+                loaders: [
+                {
+                    exclude: /node_modules/,
+                    loader: 'babel-loader'
+                },
+                {
+                    test: /\.css$/,
+                    include: [
+                        __dirname
+                    ],
+                    loader: 'css-loader'
+                },
+                {
+                    test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                    loader: 'file-loader'
+                },
+                {
+                    test: /bootstrap-sass[\/\\]assets[\/\\]javascripts[\/\\]/,
+                    loader: 'imports?jQuery=jquery'
+                }
+            ]
     },
+    plugins: [
+        new webpackUglifyJsPlugin({
+            cacheFolder: path.resolve(__dirname, './public/webpack_cached/'),
+            debug: true,
+            minimize: true,
+            sourceMap: false,
+            output: {
+                comments: false
+            },
+            compressor: {
+                warnings: false
+            }
+        })
+    ],
     devServer: {
         historyApiFallback: true,
         contentBase: './'
