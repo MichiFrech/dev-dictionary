fb320fbc86f7e75e96ba6792fb405ad43271341e
kensplanet <kensplanet@yahoo.com>
2016-10-30 22:57:57 -0400

    Reorganize the project using React - https://github.com/kensplanet/dev-dictionary/issues/9

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -2,24 +2,22 @@ import React, {Component} from 'react';
 import Search from 'react-search';
 import { connect } from 'react-redux';
 import '../../css/dev-dictionary.css';
+import $ from 'jquery';
 
 class SearchBar extends Component {
     constructor(props) {
         super(props);
 
     }
-
-    onChange(a) {
-        console.log(a);
-
+    a(x) {
+        return;
     }
 
-    render() {
 
+    render() {
         return (
             <div className="form-group">
-
-            <Search items={this.props.dictionary} onChange={this.onChange} placeholder='Enter a technology (like React)'/>
+            <Search items={this.props.dictionary} onChange={this.a}  placeholder='Enter a technology (like React)'/>
 
             </div>
         );
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,7 +4,6 @@ import DictionaryReducer from './reducer_dictionary';
 
 const rootReducer = combineReducers({
     dictionary: DictionaryReducer
-
 });
 
 export default rootReducer;
