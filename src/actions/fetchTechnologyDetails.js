ec3813ac18a944d4808e85c0eafb8fd0304068b8
Kenneth John <kensplanet@yahoo.com>
2017-02-28 13:36:23 -0500

    Use http://www.kensplanet.com/proxy to avoid cross domain issues
    https://github.com/kensplanet/dev-dictionary/issues/40

--- src/actions/fetchTechnologyDetails.js
+++ src/actions/fetchTechnologyDetails.js
@@ -1,15 +1,16 @@
 import axios from 'axios';
 
+const PROXY_URL = "http://www.kensplanet.com/proxy?url=";
+
 const TECHNOLOGY_URL = 'http://stackoverflow.com/tags/';
 
 export const FETCH_TECHNOLOGY_DETAILS = 'FETCH_TECHNOLOGY_DETAILS';
 
 export function fetchTechnologyDetails(technology) {
-    const technologyDetails = axios.get(TECHNOLOGY_URL + technology + '/info');
+    const technologyDetails = axios.get(PROXY_URL + encodeURI(TECHNOLOGY_URL + technology + '/info'));
 
     return {
         type: FETCH_TECHNOLOGY_DETAILS,
         payload: technologyDetails
     };
 }
-
