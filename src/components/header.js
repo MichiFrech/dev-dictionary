ec8c607359c34337fa81fb3b069f61451fc20dfb
kensplanet <kensplanet@yahoo.com>
2017-11-03 19:49:40 -0400

    Get version from manifest #59

--- src/components/header.js
+++ src/components/header.js
@@ -3,14 +3,13 @@ import React, {Component} from 'react';
 class Header extends Component {
     constructor(props) {
         super(props);
-
     }
 
     render() {
         return (
             <h1>
                 <img src="img/logo.png" className="logo"/>
-                <b>Dev Dictionary <small>v2.2.0</small></b>
+                <b>Dev Dictionary <small>v{chrome.runtime.getManifest().version}</small></b>
             </h1>
 
         );
